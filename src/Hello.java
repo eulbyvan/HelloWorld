30c8cee2981f41b590fb776198e27f4e7c975c1f
eulbyvan <work.issdq@gmail.com>
2022-11-18 22:45:05 +0700

    init

---
+++ src/Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, stu");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        System.out.println(myTotal);
+    }
+}
